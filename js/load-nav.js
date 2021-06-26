88c75f3b19a4723a8d8d23fb6c2cf7ce0d7047ca
Ruben Christopher Arevalo <42070061+rchrisarevalo@users.noreply.github.com>
2021-06-26 01:49:09 -0500

    Update load-nav.js

--- js/load-nav.js
+++ js/load-nav.js
@@ -7,9 +7,9 @@ function loadNav(){
         burger.classList.toggle("is-active");
 
         if (nav.classList.toggle("active") == true){
-            document.body.style.cssText = 'overflow: hidden;';
+            document.body.style.cssText = 'overflow: hidden; background-color: rgba(0, 0, 0, 0.796); transition: .5s;';
         } else{
-            document.body.style.cssText = ' ';
+            document.body.style.cssText = 'transition: .5s;';
         }
         
     });
