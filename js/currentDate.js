a21b2bf9eb000a24fe3abec4ba0ca9bae42055b5
rchrisarevalo <rubenplayer105@gmail.com>
2022-07-01 11:38:16 -0500

    Date will automatically update.

--- js/currentDate.js
+++ js/currentDate.js
@@ -6,19 +6,29 @@ today_date = new Date(2022, 4, 18)
 
 var num_days_msg = document.getElementById("num_days")
 var grad_date_msg = document.getElementById("grad_day")
+var num_days, num_hours;
+
+num_days_msg.innerHTML = [`${num_days} days`]
 
 // The total number of days from today until my expected graduation date
-var num_days = ms * (0.001 / 1) * (1 / 60) * (1 / 60) * (1 / 24)
+num_days = ms * (0.001 / 1) * (1 / 60) * (1 / 60) * (1 / 24)
 
 // The total number of hours from today until my expected graduation date
-var num_hours = ms * (0.001 / 1) * (1 / 60) * (1 / 60)
+num_hours = ms * (0.001 / 1) * (1 / 60) * (1 / 60)
 
 num_days = num_days.toFixed(0)
 num_hours = num_hours.toFixed(0)
 
-// Displays number of days left until the set date is reached on the website
-num_days_msg.innerHTML = [num_days + " days"]
+num_days_msg.innerHTML = [`${num_days} days`]
 
-if (num_days < 1){
-    num_days_msg.innerHTML = [`Graudation day in ${num_hours} hours!`]
-}
+setInterval(() => {
+    if (new Date().getHours() === 6 && new Date().getMinutes() && new Date().getSeconds() === 0) {
+        num_days = num_days - 1
+        // Displays number of days left until the set date is reached on the website
+        num_days_msg.innerHTML = [`${num_days} days`]
+    }
+
+    if (num_days < 1) {
+        num_days_msg.innerHTML = [`Graudation day in ${num_hours} hours!`]
+    }
+}, 1000)
